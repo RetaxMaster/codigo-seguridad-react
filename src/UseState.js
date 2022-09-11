81e29d863a0e9fcce2855c95f1984a4186527b98
RetaxMaster <retaxmaster@hotmail.com>
2022-09-11 16:47:54 -0500

    Adicion de useEffect a UseState

--- src/UseState.js
+++ src/UseState.js
@@ -3,6 +3,25 @@ import React from "react";
 function UseState({ name }) {
 
     const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+
+        console.log("Empezando el efecto...");
+
+        if (!!loading) {
+            setTimeout(() => {
+                console.log("Haciendo la validación");
+    
+                setLoading(false);
+    
+                console.log("Terminando la validación");
+            }, 3000);
+        }
+
+        console.log("Terminando el efecto...");
+
+    }, [loading]);
 
     return (
         <div>
@@ -13,9 +32,13 @@ function UseState({ name }) {
                 <p>Error: El código es incorrecto</p>
             )}
 
+            {loading && (
+                <p>Cargando...</p>
+            )}
+
             <input placeholder="Código de seguridad" />
             <button
-                onClick={() => setError(!error)}
+                onClick={() => setLoading(true)}
             >Comprobar</button>
         </div>
     );
