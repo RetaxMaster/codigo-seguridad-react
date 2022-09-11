e0f2d66659c1db2140285925f8005fffb104f351
RetaxMaster <retaxmaster@hotmail.com>
2022-09-11 17:31:13 -0500

    Adicion de validacion del codigo en React.Component

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Loading } from "./Loading";
 
+const SECURITY_CODE = "paradigma";
+
 class ClassState extends React.Component {
 
     constructor(props) {
@@ -8,8 +10,9 @@ class ClassState extends React.Component {
         super(props);
 
         this.state = {
+            value: "",
             error: false,
-            loading: true,
+            loading: false,
         };
 
     }
@@ -22,9 +25,18 @@ class ClassState extends React.Component {
             setTimeout(() => {
                 console.log("Haciendo la validación");
     
-                this.setState({
-                    loading: false
-                });
+                if (SECURITY_CODE === this.state.value) {
+                    this.setState({
+                        error: false,
+                        loading: false
+                    });
+                }
+                else {
+                    this.setState({
+                        error: true,
+                        loading: false
+                    });
+                }
     
                 console.log("Terminando la validación");
             }, 3000);
@@ -38,16 +50,23 @@ class ClassState extends React.Component {
                 <h2>Eliminar {this.props.name}</h2>
                 <p>Por favor escribe el código de seguridad.</p>
 
-                {this.state.error && (
+                {(this.state.error && !this.state.loading)&& (
                     <p>Error: El código es incorrecto</p>
                 )}
 
                 {this.state.loading && <Loading />}
 
-                <input placeholder="Código de seguridad" />
+                <input 
+                    placeholder="Código de seguridad"
+                    value={this.state.value}
+                    onChange={(event) => this.setState({
+                        value: event.target.value
+                    })}
+                />
+
                 <button
                     onClick={() => this.setState({
-                        loading: false
+                        loading: true
                     })}
                 >Comprobar</button>
             </div>
